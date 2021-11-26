f6e9aa31e5426e33b828be79b90ab4d7a89926b4
Anusha <anushamm77@gmail.com>
2021-11-26 22:06:58 +0530

    changes made based on feedback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   
   def index
     render plain: User.all.map{|user| user.to_pleasant_string}.join("\n")
+  end
 
   def create
     name=params[:name]
