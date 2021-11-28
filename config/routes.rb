f72874534ec86bdebfd2dee713c0009628e7aa08
Anusha <anushamm77@gmail.com>
2021-11-29 00:06:10 +0530

    Changes made based on feedback_Working application

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos
-  post "todos", to: "todos#create"
   resources :users
-  post "users", to: "users#create"
   post "users/login", to: "users#login"
 end
